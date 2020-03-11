a5e3fefdd0e6a1befb3d37a90732bc85c67adb8f
Ash Isbitt <AIsbitt@tcadmins-air-2.home>
2020-03-11 18:11:17 +0000

    create task two files

---
+++ task_2_order.py
@@ -0,0 +1,20 @@
+import os
+
+if os.path.exists("order2.txt"):
+    os.remove("order2.txt")
+
+try:
+    with open("order2.txt", 'a') as file:
+        name_list = ["Jack", "Owen", "Ianto", "Gwen", "Toshiko",
+                     "Sarah", "Martha", "Rose", "Micky", "Donna"]
+
+        for item in name_list:
+            file.write(item+"\n")
+
+    with open("order2.txt", 'r') as file:
+        rl = file.readlines()
+
+        for item in rl:
+            print(item.rstrip("\n"))
+except FileNotFoundError:
+    print("No file")
